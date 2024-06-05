7d250df626714fd4a6eff2b5040baa7df1afb988
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2024-06-05 17:01:43 -0300

    Instalando e usando Typescript (parte 1)

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Typescript
 Curso de Typescript da B7Web
+
+## Para instalar o TypeScript globalmente (Para funcionar em todos os projetos)
+`npm install -g typescript`
+
+## Verificar a versão do TypeScript
+`tsc --version`
+
+## Para compilar o arquivo
+`tsc nome_do_arquivo.ts`
--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-let numero1 = document.getElementById('numero1');
-let numero2 = document.getElementById('numero2');
-let botao = document.getElementById('calcular');
-let res = document.getElementById('resultado');
-
+var numero1 = document.getElementById('numero1');
+var numero2 = document.getElementById('numero2');
+var botao = document.getElementById('calcular');
+var res = document.getElementById('resultado');
 function calcular(n1, n2) {
-   return n1 + n2;
+    return n1 + n2;
 }
-
-botao.addEventListener('click', function() {
+botao.addEventListener('click', function () {
     res.innerHTML = calcular(numero1.value, numero2.value);
 });

bb8000467f4bdc9a9f39580b257ed0568e29de3c
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2024-06-25 15:28:27 -0300

    Os types primitivos

--- src/script.ts
+++ src/script.ts
@@ -1,12 +1,5 @@
-let numero1 = document.getElementById('numero1') as HTMLInputElement;
-let numero2 = document.getElementById('numero2') as HTMLInputElement;
-let botao = document.getElementById('calcular') as HTMLElement;
-let res = document.getElementById('resultado') as HTMLElement;
+let nome: string = 'Gustavo';
 
-function calcular(n1: number, n2: number) {
-   return n1 + n2;
-}
+let idade: number = 90;
 
-botao.addEventListener('click', function() {
-    res.innerHTML = calcular(+numero1.value, +numero2.value).toString();
-});
+let programador: boolean = true;

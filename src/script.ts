bfe06f29b8fa10d211eea1f8b8cfb705081930b6
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2024-07-04 00:14:45 -0300

    Usando types no retorno de uma função

--- src/script.ts
+++ src/script.ts
@@ -1,6 +1,12 @@
-function firstLetterUpperCase(name: string) {
+function firstLetterUpperCase(name: string): string {
     let firstLetter = name.charAt(0).toUpperCase();
     return firstLetter + name.substring(1);
 }
 
-firstLetterUpperCase('gustavo');
+let nome: string = firstLetterUpperCase('gustavo');
+
+function somar(n1: number, n2: number): number {
+    return n1 + n2;
+}
+
+let algo = somar(10, 20);
